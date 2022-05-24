2df0c4add1e24b4d90307f1647b2d05ca8b2a5a7
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-05-24 22:03:22 +0200

    some changes

---
+++ src/components/Events/Event/Event.js
@@ -0,0 +1,40 @@
+import styles from './Event.module.scss';
+import { v4 as uuidv4 } from 'uuid';
+import RangeEvent from "../RangeEvent/RangeEvent";
+import TimeEvent from "../TimeEvent/TimeEvent";
+import BaseEvent from "../../reusables/BaseEvent/BaseEvent";
+
+// for a specific time we can have more than 2
+function Event(props) {
+    const event = props.event;
+
+    let eventComponent;
+    if (event.starts && event.ends) {
+        eventComponent = (
+            <RangeEvent starts={event.starts} ends={event.ends} title={event.title} />
+        );
+    }
+    else if (event.starts) {
+        eventComponent = (
+            <TimeEvent starts={event.starts} title={event.title} />
+        );
+    }
+    else {
+        eventComponent = (
+            <BaseEvent title={event.title} />
+        );
+    }
+    
+    console.log(eventComponent);
+
+    ///////////////////////////////////
+    // JSX
+    ///////////////////////////////////
+    return (
+        <div className={styles['event']}>
+            {eventComponent}
+        </div>
+    );
+}// Event
+
+export default Event;
