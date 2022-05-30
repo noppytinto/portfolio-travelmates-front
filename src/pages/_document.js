ec66d999411770712ad675d3f4788346ea24defd
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-05-30 22:35:32 +0200

    move font import to _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,22 +6,24 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      	{/* All your global head tags such as meta, link, etc. will go here*/}
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-        <meta name="description" content="Travel Planner app" />
-        <link rel="icon" href="/favicon.ico" />
-        {/* <link rel="apple-touch-icon" href="/logo192.png" /> */}
-        {/* <link rel="manifest" href="/manifest.json" /> */}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                {/* All your global head tags such as meta, link, etc. will go here*/}
+                {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+                <meta name="description" content="Travel Planner app" />
+                <link rel="icon" href="/favicon.ico" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" 
+                      rel="stylesheet"></link>
+                {/* <link rel="apple-touch-icon" href="/logo192.png" /> */}
+                {/* <link rel="manifest" href="/manifest.json" /> */}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
 
 export default Document;
