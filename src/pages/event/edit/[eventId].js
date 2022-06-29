344a916470578f1a332f6a357f8f9a1ef34f0b4d
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-06-29 23:10:03 +0200

    Update [eventId].js

--- src/pages/event/edit/[eventId].js
+++ src/pages/event/edit/[eventId].js
@@ -1,24 +1,24 @@
-// import * as assets from '../../utils/assets-manager';
-// import styles from './TimelinePage.module.scss';
-// import {useSelector} from "react-redux";
-// import {useRouter} from "next/router";
-//
-//
-// function EditEventPage(props) {
-//     const router = useRouter();
-//     const eventId = router.query.eventId;
-//     const event = useSelector(state => state.userSlice.events.get(eventId));
-//
-//     ///////////////////////////////////
-//     // JSX
-//     ///////////////////////////////////
-//     return (
-//         <div className={styles['event-page']}>
-//             <p>{event.name}</p>
-//         </div>
-//     );
-// }// EditEventPage
-//
-//
-//
-// export default EditEventPage;
+import * as assets from '../../../utils/assets-manager';
+import styles from './TimelinePage.module.scss';
+import {useSelector} from "react-redux";
+import {useRouter} from "next/router";
+
+
+function EditEventPage(props) {
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    const event = useSelector(state => state.userSlice.events.get(eventId));
+
+    ///////////////////////////////////
+    // JSX
+    ///////////////////////////////////
+    return (
+        <div className={styles['event-page']}>
+            <p>{event.name}</p>
+        </div>
+    );
+}// EditEventPage
+
+
+
+export default EditEventPage;
