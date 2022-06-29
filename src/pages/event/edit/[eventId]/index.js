2b480f1b5f53864675621eea64e5d615c94a49c6
noppytinto <34626569+noppytinto@users.noreply.github.com>
2022-06-29 23:17:37 +0200

    bugfix

--- src/pages/event/edit/[eventId]/index.js
+++ src/pages/event/edit/[eventId]/index.js
@@ -1,20 +1,20 @@
-import * as assets from '../../../utils/assets-manager';
-import styles from './TimelinePage.module.scss';
-import {useSelector} from "react-redux";
+// import * as assets from '../../../../utils/assets-manager';
+import styles from './EditEventPage.module.scss';
+// import {useSelector} from "react-redux";
 import {useRouter} from "next/router";
 
 
 function EditEventPage(props) {
     const router = useRouter();
     const eventId = router.query.eventId;
-    const event = useSelector(state => state.userSlice.events.get(eventId));
+    // const event = useSelector(state => state.userSlice.events.get(eventId));
 
     ///////////////////////////////////
     // JSX
     ///////////////////////////////////
     return (
         <div className={styles['event-page']}>
-            <p>{event.name}</p>
+            <p>{eventId}</p>
         </div>
     );
 }// EditEventPage
